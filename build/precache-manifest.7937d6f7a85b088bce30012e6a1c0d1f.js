fe902e6dc4055ad4c7208504ca9c8b3a854570fc
Macintyre Sunde <imacsunde@gmail.com>
2023-03-08 17:44:07 -0500

    added build to repo

---
+++ build/precache-manifest.7937d6f7a85b088bce30012e6a1c0d1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e9c8d4dd82e709ab42d",
+    "url": "/static/css/main.1cd1067c.chunk.css"
+  },
+  {
+    "revision": "5e9c8d4dd82e709ab42d",
+    "url": "/static/js/main.5e9c8d4d.chunk.js"
+  },
+  {
+    "revision": "a1fee0e1cd10b8944767",
+    "url": "/static/js/1.a1fee0e1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b45f53d2fa65073274379fe513853de5",
+    "url": "/index.html"
+  }
+];
