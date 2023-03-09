d5121a04ad0aab457ed6d72691bb3ee2cb16900d
Macintyre Sunde <imacsunde@gmail.com>
2023-03-08 22:06:04 -0500

    updated bio

--- src/Person.js
+++ src/Person.js
@@ -9,7 +9,7 @@ const macintyre = {
     profile: profile,
     name: "Macintyre Sunde",
     title: "Software Developer and Creative",
-    bio: "Macintyre is a recent graduate from Haverford College with academic and personal experience in software development, computer graphics and technical art. They majored in Computer Science at Haverford and took many courses in visual studies and traditional arts. They are driven by the mission to help people and the planet with their technical knowledge and creative spirit.",
+    bio: "Macintyre is a recent graduate from Haverford College with academic and personal experience in software development, computer graphics and technical art. They majored in Computer Science and took many courses in visual studies and traditional arts. They are driven by the mission to help people and the planet with their technical knowledge and creative spirit.",
     socials: [
         {
             link: "https://www.linkedin.com/in/macintyre-sunde-82b13b1a6/",
